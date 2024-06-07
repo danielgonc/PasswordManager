872a4ad4641d74812f9071f3a895d2e1b0b9e634
danielgonc <danielgon131@gmail.com>
2024-06-07 01:07:18 -0300

    added cards to home page

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,26 +2,37 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PasswordsService } from './passwords.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, NgFor],
+  imports: [RouterOutlet, NavbarComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'Password manager';
-  passwords : any = [];
+  passwords: any = [];
 
-  constructor(private passwordService: PasswordsService) { }  
+  constructor(private passwordService: PasswordsService) { }
 
   fetchPasswords(): void {
     this.passwordService.getPasswords().subscribe((data: any) => {
       this.passwords = data;
     });
   }
+
+  parseFavicon(url: string): string {
+    if (url.indexOf('http') > -1) {
+      const { hostname } = new URL(url);
+
+      return `//${hostname}/favicon.ico`;
+    }
+
+    return "";
+  }
+
   ngOnInit() {
     this.fetchPasswords();
   }
